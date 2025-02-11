468611b49b22ce293deaecf7cbc8cc5ff99aa835
Jh <473380917@qq.com>
2025-02-11 10:03:35 +0800

    首页功能入口

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,11 @@ export default function Home() {
     return (
             <div
                 className="w-full flex-1 flex gap-8 justify-center items-center h-full font-[family-name:var(--font-geist-sans)]">
-                <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-
+                <div className="flex gap-4 p-5 flex-wrap row-start-2 items-center sm:items-start">
                     <Link className="p-10 rounded-sm bg-red-100 text-5xl font-bold" href={`/liuren`}>六壬</Link>
+                    <Link className="p-10 rounded-sm bg-red-100 text-5xl font-bold" href={`/liuren`}>干支</Link>
+                    <Link className="p-10 rounded-sm bg-blue-100 text-5xl font-bold" href={`/liuren`}>子午流注</Link>
+                    <Link className="p-10 rounded-sm bg-green-100 text-5xl font-bold" href={`/liuren`}>灵龟八法</Link>
                 </div>
             </div>
 
